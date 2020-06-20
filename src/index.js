906049b629e13807c06c249f7c95ca85eef81582
jamilurrahmanmuhammad <jamil.muhammad@gmail.com>
2020-06-20 20:04:21 +0300

    fixed a warning

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Room from './Room.js';
 
-const Hi=({name,age}) =>  <div>{"Name: "+ name }{ ", Age:" + age} </div>
+//const Hi=({name,age}) =>  <div>{"Name: "+ name }{ ", Age:" + age} </div>
 
 
 
 ReactDOM.render(<Room/>,
  document.querySelector('#root'));
+
