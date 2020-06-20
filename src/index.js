9cb5b0da16eaf67b2dbfc7c1177997b13d688112
jamilurrahmanmuhammad <jamil.muhammad@gmail.com>
2020-06-20 18:46:12 +0300

    committing practice code and yarn build together

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hi=({name,age}) =>  <div>{"Name: "+ name }{ ", Age:" + age} </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+ReactDOM.render(<Hi name='Jamil Muhammad' age={10+8}></Hi>,
+ document.querySelector('#root'));
